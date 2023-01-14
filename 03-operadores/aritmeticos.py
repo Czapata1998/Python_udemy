25738cbb470c724b945efd0f0787fbbc7c179bde
Czapata1998 <cristian1.zapata@ucp.edu.co>
2023-01-14 11:59:21 -0500

    actualizado

--- 03-operadores/aritmeticos.py
+++ 03-operadores/aritmeticos.py
@@ -23,19 +23,25 @@ print("****EJERCICIO 1****")
 # Elabora un programa que lea 2 numeros enteros positivos
 # y que muestre la suma y la multiplicacion de estos.
 
-numero1 = int(input("Ingrese un numero porfavor: "))
-numero2 = int(input("Ingrese otro numero porfavor: "))
+# numero1 = int(input("Ingrese un numero porfavor: "))
+# numero2 = int(input("Ingrese otro numero porfavor: "))
 
-operacion = int(input("\n- Ingrese 1 si quiere sumar: \n- Ingrese 2 si quiere restar: "))
+# operacion = int(input("\n- Ingrese 1 si quiere sumar: \n- Ingrese 2 si quiere restar: "))
 
-if operacion == 1:
-    operacion = numero1 + numero2
-    print(f"la suma de {numero1} + {numero2} = {operacion}")
-elif operacion == 2:
-    operacion = numero1 - numero2
-    print(f"la suma de {numero1} - {numero2} = {operacion}")
-else:
-    print("Ingrese un valor valido")
+# if operacion == 1:
+#     operacion = numero1 + numero2
+#     print(f"la suma de {numero1} + {numero2} = {operacion}")
+# elif operacion == 2:
+#     operacion = numero1 - numero2
+#     print(f"la suma de {numero1} - {numero2} = {operacion}")
+# else:
+#     print("Ingrese un valor valido")
 
 
+print("****EJERCICIO 2****")
 
+# Suponga que un individuo desea invetir su capital en un banco y desea saber cuanto dinero
+# ganará despues de un mes si en el banco paga a razon de 2% mensual
+
+
+inversion = int(input("Ingrese el monto que desea invertir: "))
