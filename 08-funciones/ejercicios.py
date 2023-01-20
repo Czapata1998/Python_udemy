337612ead586a0672cc4af6659f0ece7a0890753
Czapata1998 <cristian1.zapata@ucp.edu.co>
2023-01-20 16:58:47 -0500

    Me gusta la tula

---
+++ 08-funciones/ejercicios.py
@@ -0,0 +1,36 @@
+"""
+Programa que tome 3 notas de un estudiante y diga la nota final del curso.
+Tenga en cuenta que la primera y segunda nota valen el 30% de la nota final. La tercera nota vale el 40%
+"""
+# def calcularNota(nota_1, nota_2, nota_3):
+#     return (nota_1*0.3) + (nota_2*0.3) + (nota_3*0.4)
+
+# nota_1 = float(input("Ingresa la primer nota del estudiante: "))
+# nota_2 = float(input("Ingresa la segunda nota del estudiante: "))
+# nota_3 = float(input("Ingresa la tercer nota del estudiante: "))
+
+# notaFinal = calcularNota(nota_1, nota_2, nota_3)
+
+# if notaFinal <= 5.9:
+#     print("El estudiante reprobó")
+# elif notaFinal >= 6 and notaFinal <= 8:
+#     print("El estudiante pasó bien")
+# elif notaFinal >= 8.1 and notaFinal <= 10:
+#     print("El estudiante sacó nota sobresaliente")
+    
+
+# print(f"la nota final del estudiante es {notaFinal}")
+
+
+"""
+Programa que calcule el IVA de una compra del 19% sobre el valor total de la compra
+"""
+# def calcularIva(compra):
+#      iva = compra *0.19
+#      return iva
+
+# compra =  float(input("Ingrese el valor de su compra: "))
+
+# iva = calcularIva(compra)
+
+# print(f"el precio de la compra sin iva es de {compra} y mas el iva es {compra+iva}")
