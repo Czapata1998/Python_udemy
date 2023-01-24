a09bd75660e648082224b5184635bdb23a8d4012
Czapata1998 <cristian1.zapata@ucp.edu.co>
2023-01-24 17:58:06 -0500

    ejercicio 2

--- 11-ejercicios/ejercicio1.py
+++ 11-ejercicios/ejercicio1.py
@@ -44,14 +44,41 @@ print("##### RECORRER Y MOSTRAR #####")
 #     print(numero)
     
 print(mostrarLista(numeros))
-print(mostrarLista(["Kevin", "Jovy", "Cristhian"]))
+print(mostrarLista(["Kevin", "Andres", "Cristhian"]))
 
 #Oedenar y mostrar    
 print("##### ORDENAR Y MOSTRAR #####")
 
 numeros.sort()
-
 print(mostrarLista(numeros))
+
+
+#Mostrar su longitud    
+print("##### MOSTRAR SU LONGITUD #####")
+
+print(len(numeros))
+
+
+#Buscar un elemento que el cliente pida por teclado    
+print("##### Busqueda en la lista #####")
+
+
+busqueda = int(input("Introduce el número: "))
+
+comprobar = isinstance(busqueda, int)
+
+while not comprobar or busqueda <= 0:
+    busqueda = int(input("Introduce el número: "))
+else:
+    print(f"Haz introducido el {busqueda}")
+    
+print(f"### Buscar en la lista el número {busqueda} ###")
+
+search = numeros.index(busqueda)
+
+print(f"el numero buscado existe en la lista, es el indice {search}")
+    
+
 
 
 
