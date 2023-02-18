47731b8f7f9957b69c1f5775aefd74478aadf413
Czapata1998 <cristhianjulian22@gmail.com>
2023-02-17 23:47:48 -0500

    nuevos ejercicios

--- 16-POO/main.py
+++ 16-POO/main.py
@@ -9,3 +9,46 @@
 #OCULTACION: Cada objeto tiene sus datos en concreto y solo deben modificarse desde dentro de la clase, y para esto es importante tener dentro de la clase los metodos getter y seterr
 
 
+#Definir una clase (Molde para crear mas objetos de ese tipo)
+
+class Moto: 
+    #Atributos o propiedads (Variables)
+    #caracteristicas del coche
+    color = "Verde"
+    marca = "Victory"
+    modelo = "Bomber"
+    velocidad = 300
+    caballaje = 200
+    plazas = 4
+    
+    #Metodos, son acciones que hace el objeto (coche) (funciones)
+    
+    def acelerar(self):
+        self.velocidad += 3
+    
+    def frenar(self):
+        self.velocidad -= 8
+        
+    def getvelocidad(self):
+        return self.velocidad
+    
+    
+    #Fin definicion clase
+    
+    #Crear un objeto o instanciar la clase
+    
+coche = Moto() 
+    
+print(coche.marca, coche.color)
+print(f"La velocidad de la moto es: {coche.velocidad}")
+    
+coche.acelerar()
+coche.acelerar() # Llamamos a las funciones o metodos para modificar la funcion del objeto
+coche.acelerar()
+coche.acelerar()
+
+coche.frenar()
+coche.frenar()
+coche.frenar()
+
+print(f"La velocidad de la moto es: {coche.velocidad}")
