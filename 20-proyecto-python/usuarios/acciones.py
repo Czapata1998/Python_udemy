57cbbcf0e2d494bfba4a68098df18cfe27c758e0
Czapata1998 <cristhianjulian22@gmail.com>
2023-02-24 00:14:28 -0500

    registro completado

--- 20-proyecto-python/usuarios/acciones.py
+++ 20-proyecto-python/usuarios/acciones.py
@@ -1,3 +1,5 @@
+import usuarios.usuario as modelo
+
 class Acciones:
 
     def registro(self):
@@ -8,6 +10,13 @@ class Acciones:
         email = input("Digite por favor su correo electronico: ")
         password = input("Ingresa tu contrasena: ")
         
+        usuario = modelo.Usuario(nombre, apellidos, email, password)
+        registro = usuario.registrar()
+        
+        if registro [0] >= 1:
+            print(f"perfecto {registro[1].nombre} te has registrado con el correo {registro[1].email}")
+        else:
+            print("No te has registrado correctamente")
     
     def login(self):
         print("\nOk. identificate en el sistema")
--- 20-proyecto-python/usuarios/usuario.py
+++ 20-proyecto-python/usuarios/usuario.py
@@ -1,3 +1,16 @@
+import mysql.connector
+import datetime
+
+database = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "",
+    database = "master_python",
+    port = 3306
+)
+
+cursor = database.cursor(buffered=True)
+
 class Usuario:
     
     def __init__(self, nombre, apellidos, email, password):
@@ -7,7 +20,14 @@ class Usuario:
         self.password = password
         
     def registrar(self):
-        return self.nombre
+        fecha = datetime.datetime.now()
+        sql = "INSERT INTO  usuarios VALUES(null, %s, %s, %s, %s, %s)"
+        usuario = (self.nombre, self.apellidos, self.email, self.password, fecha)
+        
+        cursor.execute(sql, usuario)
+        database.commit()
+        
+        return[cursor.rowcount, self]
         
         
     def identificar(self):
