39a9a5fea369b5889187e6c68dbc97520254402f
hackermen <cristian1.zapata@ucp.edu.co>
2023-03-13 12:08:06 -0500

    avances tk

--- 21-tkinter/13-ejercicioTk.py
+++ 21-tkinter/13-ejercicioTk.py
@@ -3,7 +3,7 @@ Crear un programa que tenga:
 - Ventana (Realizado)
 - Tamaño fijo (Realizado)
 - No redimensionable (Realizado)
-- Un menú
+- Un menú -Inicio, añadir, informacion, salir 
 - Diferentes pantallas
 - Formulario de añadir productos
 - Guardar datos temporalmente 
@@ -13,10 +13,22 @@ Crear un programa que tenga:
 
 from tkinter import *
 
+#Definir la ventana
 ventana = Tk()
 ventana.title("Ejercicio final - tkinter")
 ventana.geometry("500x500")
 ventana.resizable(0, 0)
 
 
+# Menú superior
+menu_superior = Menu(ventana)
+menu_superior.add_command(label="Inicio")
+menu_superior.add_command(label="Añadir")
+menu_superior.add_command(label="Informacion")
+menu_superior.add_command(label="salir")
+
+ventana.config(menu=menu_superior)
+
+
+#Cargar ventana
 ventana.mainloop()
