e94791211bbbc691c2af3b4063e248e06de408f7
Czapata1998 <122549554+Czapata1998@users.noreply.github.com>
2023-05-18 22:49:13 -0500

    panel de administracion

--- 25-django/AprendiendoDjango/miapp/models.py
+++ 25-django/AprendiendoDjango/miapp/models.py
@@ -3,16 +3,25 @@ from django.db import models
 # Create your models here.
 
 class Article(models.Model):
-    title = models.CharField(max_length=150)
-    content = models.TextField()
-    image = models.ImageField(default='null')
-    public = models.BooleanField()
-    create_at = models.DateTimeField(auto_now_add=True) #registra la fecha de creacion
-    update_at = models.DateTimeField(auto_now=True) #Registra la fecha de modificacion
+    title = models.CharField(max_length=150, verbose_name='Titulo')
+    content = models.TextField(verbose_name='Contenido')
+    image = models.ImageField(default='null', verbose_name='Imagenes')
+    public = models.BooleanField(verbose_name='Publico?')
+    create_at = models.DateTimeField(auto_now_add=True, verbose_name='Fecha creacion') #registra la fecha de creacion
+    update_at = models.DateTimeField(auto_now=True, verbose_name='Actualizacion') #Registra la fecha de modificacion
     
-    
+    class Meta:
+        verbose_name = 'Articulo'
+        verbose_name_plural = 'Articulos'
+        verbose_name_plural = 'Articulos'
+        ordering = ['create_at']
+        
     
 class Category(models.Model):
     name= models.CharField(max_length=150)
     description= models.CharField(max_length=500)
     create_at= models.DateField()
+    
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
