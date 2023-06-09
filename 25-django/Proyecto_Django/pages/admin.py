6e78d3fb517c286f08dad97a4ccb6ed1629fdf22
Czapata1998 <122549554+Czapata1998@users.noreply.github.com>
2023-06-08 22:36:37 -0500

    nuevos procesos

--- 25-django/Proyecto_Django/pages/admin.py
+++ 25-django/Proyecto_Django/pages/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Page)
+
+#Configuracion del panel
+title = "Proyecto de Django"
+subtitle = "Panel de gestion"
+
+admin.site.site_header = title
+admin.site.site_title = title
+admin.site.index_title = subtitle
--- 25-django/Proyecto_Django/pages/views.py
+++ 25-django/Proyecto_Django/pages/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Page
 
 # Create your views here.
+
+def page(request, slug):
+    
+    page = Page.objects.get(slug=slug)
+    
+    return render(request, 'page/page.html',{
+        
+        "page": page
+        
+    })
+                  
