197708ebb9d89b99ef02baa78f27291950b43f56
Czapata1998 <122549554+Czapata1998@users.noreply.github.com>
2023-07-30 14:02:29 -0500

    nuevos avances domingo

--- 25-django/Proyecto_Django/blog/urls.py
+++ 25-django/Proyecto_Django/blog/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path('articulos/', views.articles, name="list_articles"),
+    path('categoria/<int:category_id>/', views.category, name='category'),
 ]
+
 
 
 
--- 25-django/Proyecto_Django/blog/views.py
+++ 25-django/Proyecto_Django/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Category, Article
 # Create your views here.
 def articles(request):
@@ -9,4 +9,16 @@ def articles(request):
     return render(request, 'articles/list.html', {
         'title': 'Articulos',
         'articles' : articles
+    })
+    
+def category(request, category_id):
+    # Intentamos obtener la categoría con el ID proporcionado
+    category = get_object_or_404(Category, id=category_id)
+    
+    # Obtener los artículos asociados a esta categoría
+    articles = category.article_set.all()  # Asumiendo que la relación se llama 'article_set'
+    
+    return render(request, 'categories/category.html', {
+        'category': category,
+        'articles': articles,
     })
