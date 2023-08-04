13d69aa5440c3d71d27987148908a48d366c1825
hackermen <cristian1.zapata@ucp.edu.co>
2023-08-04 13:31:56 -0500

    progreso 4 de agosto

--- 25-django/Proyecto_Django/mainapp/views.py
+++ 25-django/Proyecto_Django/mainapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from mainapp.forms import RegisterForm
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
@@ -25,8 +27,9 @@ def register_page(request):
         
         if register_form.is_valid():
             register_form.save()
+            messages.success(request, 'Te has registrado correctamente!!')
             
-            return redirect('inicio/')
+            return redirect('inicio')
     
     
     return render(request, 'users/register.html', {
@@ -34,3 +37,26 @@ def register_page(request):
         'title': 'Registro',
         'register_form': register_form
     })
+    
+    
+def login_page(request):
+    
+    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username = username, password = password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('inicio')
+        
+        else:
+            messages.warning(request, 'No te haz podido identificar :(')
+            
+        
+    return render(request, 'users/login.html', {
+        'title' : 'identificacion'
+    })
+    
