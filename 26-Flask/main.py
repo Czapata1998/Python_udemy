5ba3affce62b4cde7023caf4fcfdc46615271d13
hackermen <cristian1.zapata@ucp.edu.co>
2023-08-09 19:55:29 -0500

    nuevos avances flask

--- 26-Flask/main.py
+++ 26-Flask/main.py
@@ -1,9 +1,18 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, flash, redirect, url_for, render_template, request
 from datetime import datetime
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
+app.secret_key = 'clave_secreta_flask'
+# Conexion Db
 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'proyectoflask'
+
+mysql = MySQL(app)
 # Context procesor 
 
 @app.context_processor
@@ -53,5 +62,46 @@ def contacto(redirecion = None):
 def lenguajes():
     return render_template ('lenguajes.html')
 
+@app.route('/crear-coche', methods=['GET', 'POST'])
+def crear_coche():
+    
+    if request.method == 'POST':
+        
+        marca = request.form['marca']
+        modelo = request.form['modelo']
+        precio = request.form['precio']
+        ciudad= request.form['ciudad']
+        
+        
+        cursor = mysql.connection.cursor()
+        cursor.execute("INSERT INTO coches VALUES(NULL, %s, %s,  %s, %s)", (marca, modelo, precio, ciudad))
+        cursor.connection.commit()
+        flash("Has creado el coche correctamente")
+    
+        return redirect(url_for('index'))
+        
+    return render_template('crear_coche.html')
+
+
+@app.route('/coches')
+def coches():
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM coches")
+    coches = cursor.fetchall()
+    cursor.close()
+    
+    return render_template('coches.html', coches=coches)
+
+@app.route('/coche/<int:coche_id>')
+def coches():
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM coches")
+    coches = cursor.fetchall()
+    cursor.close()
+    
+    return render_template('coches.html', coches=coches)
+    
 if __name__ == '__main__':
     app.run(debug=True)
+    
+    
